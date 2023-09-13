da30bbc8e3a012db9deb1b081b09707f7fe45aa6
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-09-13 17:49:51 -0300

    sql - repositorys and new data

---
+++ src/main/resources/Import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_department (name) VALUES ('Tech');
+INSERT INTO tb_department (name) VALUES ('Pet');
+
+INSERT Into tb_product (name,price,department_id) VALUES ('Macbook Pro', 4000.0,1);
+INSERT Into tb_product (name,price,department_id) VALUES ('PC Gamer', 5000.0,1);
+INSERT Into tb_product (name,price,department_id) VALUES ('Pet House', 300.0,2);
